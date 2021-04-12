0cc2872eaf99f84f3678e56c807dfb84e70239f9
Elizabethproyecto12 <82402881+Elizabethproyecto12@users.noreply.github.com>
2021-04-12 12:34:18 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,5 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    
+    apiKey: "AIzaSyAnmgNLEThL5GAIvUZt_xfrxg_5Hf7i65k",
+    authDomain: "proyectoelizbeth43m.firebaseapp.com",
+    projectId: "proyectoelizbeth43m",
+    storageBucket: "proyectoelizbeth43m.appspot.com",
+    messagingSenderId: "873571952153",
+    appId: "1:873571952153:web:43e34dee9a1dab5e83ac88",
+    measurementId: "G-M8YTCRN6TZ"
 });
